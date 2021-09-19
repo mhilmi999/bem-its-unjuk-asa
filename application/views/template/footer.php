bfd453c1ef1947fc513a3140936319358227a5f4
mhilmi999 <hilmi.muhammad.rmdhn@gmail.com>
2021-09-19 16:19:17 +0700

    footer desktop ok, kurang sedikit di mobile icon

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,34 +1,35 @@
 <footer class="text-center text-white" style="background-color: #393999;">
     <div class="px-5 py-5 row align-items-start">
-        <div class="col-6 d-none d-sm-block d-md-block d-lg-block">
-            <div class="col ps-5 fw-normal text-start">
+        <div class="col d-none d-xl-block d-xxl-none">
+            <div class="ps-5 col-8 fw-normal text-start">
                 <p class="fs-6">Sekretariat BEM ITS:</p>
                 <p class="fs-6">SCC ITS, Keputih Sukolilo Kota SBY,<br>Keputih, Mulyorejo, Surabaya City, East Java<br>60115</p>
             </div>
-            <iframe class="rounded  w-75 h-auto" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6244300710227!2d112.79141761396924!3d-7.283502273602015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa1344f5802f%3A0x110c1b2a70e34295!2sSekretariat%20BEM%20ITS!5e0!3m2!1sen!2sid!4v1632029136219!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe class="ps-5 d-flex rounded  w-75 h-auto" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6244300710227!2d112.79141761396924!3d-7.283502273602015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa1344f5802f%3A0x110c1b2a70e34295!2sSekretariat%20BEM%20ITS!5e0!3m2!1sen!2sid!4v1632029136219!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
 
         <!-- icon sosmed -->
-        <div class="col-6">
+        <div class="col-4">
             <!-- Section: Social media -->
-            <div class="col ps-5 fw-normal text-start d-none d-sm-block d-md-block d-lg-block">
+            <div class="col ps-3 fw-normal text-start d-none d-xl-block d-xxl-none">
                 <p class="fs-6">Email:<br>bem@its.ac.id<br>Phone:<br>08XXXXXXXXXXX</p>
                 <p class="fs-6">Get in touch with us:</p>
             </div>
-            <div class>
-                <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-line"></i></a>
-    
+            <div class="col-sm-12 d-flex justify-content-start justify-content-sm-center ">
+
+                <a class="btn btn-link btn-floating btn-lg text-dark float-start " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-line"></i></a>
+
                 <!-- Twitter -->
-                <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-twitter"></i></a>
-    
+                <a class="btn btn-link btn-floating btn-lg text-dark float-start " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-twitter"></i></a>
+
                 <!-- Yt -->
-                <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-youtube-square"></i></a>
-    
+                <a class="btn btn-link btn-floating btn-lg text-dark float-start " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-youtube-square"></i></a>
+
                 <!-- Instagram -->
-                <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-instagram"></i></a>
-    
+                <a class="btn btn-link btn-floating btn-lg text-dark float-start " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-instagram"></i></a>
+
                 <!-- Linkedin -->
-                <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-linkedin"></i></a>
+                <a class="btn btn-link btn-floating btn-lg text-dark float-start " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-linkedin"></i></a>
             </div>
             <!-- Line -->
 
