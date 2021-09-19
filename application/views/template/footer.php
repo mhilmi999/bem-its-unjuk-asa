3632f55e9cf6957c3387661f685951b939660952
mhilmi999 <hilmi.muhammad.rmdhn@gmail.com>
2021-09-19 14:01:14 +0700

    display sosmed only when on mobile footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,9 +1,9 @@
 <footer class="text-center text-white" style="background-color: #393999;">
     <div class="px-5 py-5 row align-items-start">
-        <div class="col-6 d-none d-sm-block">
+        <div class="col-6 d-none d-sm-block d-md-block d-lg-block">
             <div class="col ps-5 fw-normal text-start">
-                <p>Sekretariat BEM ITS :</p>
-                <p>SCC ITS, Keputih Sukolilo Kota SBY,<br>Keputih, Mulyorejo, Surabaya City, East Java<br>60115</p>
+                <p class="fs-6">Sekretariat BEM ITS:</p>
+                <p class="fs-6">SCC ITS, Keputih Sukolilo Kota SBY,<br>Keputih, Mulyorejo, Surabaya City, East Java<br>60115</p>
             </div>
             <iframe class="rounded  w-75 h-auto" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6244300710227!2d112.79141761396924!3d-7.283502273602015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa1344f5802f%3A0x110c1b2a70e34295!2sSekretariat%20BEM%20ITS!5e0!3m2!1sen!2sid!4v1632029136219!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
@@ -11,9 +11,9 @@
         <!-- icon sosmed -->
         <div class="col-6">
             <!-- Section: Social media -->
-            <div class="col ps-5 fw-normal text-start">
-                <p>Email:<br>bem@its.ac.id<br>Phone:<br>08XXXXXXXXXXX</p>
-                <p>Get in touch with us:</p>
+            <div class="col ps-5 fw-normal text-start d-none d-sm-block d-md-block d-lg-block">
+                <p class="fs-6">Email:<br>bem@its.ac.id<br>Phone:<br>08XXXXXXXXXXX</p>
+                <p class="fs-6">Get in touch with us:</p>
             </div>
             <div class>
                 <a class="btn btn-link btn-floating btn-lg text-dark " href="#!" role="button" data-mdb-ripple-color="dark"><i style="color: #fcfae6;" class="fab fa-line"></i></a>
