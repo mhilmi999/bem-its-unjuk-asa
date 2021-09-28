4049efe6606699292b6985eadcb28e2816ab7b4d
mhilmi999 <hilmi.muhammad.rmdhn@gmail.com>
2021-09-28 19:34:06 +0700

    fix security ig integration, not yet design

--- application/views/template/home.php
+++ application/views/template/home.php
@@ -347,22 +347,7 @@
 
 
 <script src="https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0rc1/src/instafeed.min.js"></script>
-<!-- <script src="<?= base_url();?>assets/js/custom.js"></script> -->
-
-<script type="text/javascript">
-  var userFeed = new Instafeed({
-    get: 'user',
-    target: "instafeed",
-    resolution: 'standard_resolution',
-    accessToken: 'IGQVJXNlVHVjVHd3MzN1ZAqU1NQdlVFTWRUdEVDU0M4U3VuUFRKcnE3b2ItTjVLZAzZAZAZAXJad2xYX2o5dXRHdkpZAY0JtMWJsMnpHT0VUTzFGZAFpQekFKTmtNMWJhRXBySUcwcnlKbUZAfRGxSSVZAtV3l3SQZDZD',
-    limit: 8,
-
-    template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-  });
-  userFeed.run();
-</script>
-
-
+<script src="<?= base_url();?>assets/js/custom.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,10 @@
-$(document).ready(function () {
+var userFeed = new Instafeed({
+	get: 'user',
+	target: "instafeed",
+	resolution: 'standard_resolution',
+	accessToken: 'IGQVJXNlVHVjVHd3MzN1ZAqU1NQdlVFTWRUdEVDU0M4U3VuUFRKcnE3b2ItTjVLZAzZAZAZAXJad2xYX2o5dXRHdkpZAY0JtMWJsMnpHT0VUTzFGZAFpQekFKTmtNMWJhRXBySUcwcnlKbUZAfRGxSSVZAtV3l3SQZDZD',
+	limit: 8,
 
-	var userFeed = new Instafeed({
-		get: 'user',
-		target: "instafeed",
-		resolution: 'standard_resolution',
-		accessToken: 'IGQVJXNlVHVjVHd3MzN1ZAqU1NQdlVFTWRUdEVDU0M4U3VuUFRKcnE3b2ItTjVLZAzZAZAZAXJad2xYX2o5dXRHdkpZAY0JtMWJsMnpHT0VUTzFGZAFpQekFKTmtNMWJhRXBySUcwcnlKbUZAfRGxSSVZAtV3l3SQZDZD',
-		limit: 8,
-
-		template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-	});
-	userFeed.run();
-
-
+	template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 });
+userFeed.run();
